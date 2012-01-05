e0836242373668b1d0a5a42a5ae7101f079074e5
Artyom Grigoriev <ortemij@gmail.com>
2012-01-05 15:41:14 +0400

    local-properties.php for main site

--- main/config/local-properties.php
+++ main/config/local-properties.php
@@ -1,6 +1,17 @@
 <?php
 
 global $PROPERTIES;
-$PROPERTIES['main_site_url'] = "local.pipeinpipe.info";
+
+$PROPERTIES['db']['username']  = "pipedb";
+$PROPERTIES['db']['password']  = "fd6zSDrUdyFvm7BK";
+$PROPERTIES['db']['database']  = "pipeinpipe_utf8";
+
+$PROPERTIES['cookies']['domain'] = ".pipeinpipe.info";
+
+$PROPERTIES['main_site_url']   = "pipeinpipe.info";
+$PROPERTIES['cupms_site_url']  = "cupms.pipeinpipe.info";
+$PROPERTIES['mobile_site_url'] = "m.pipeinpipe.info";
+
+$PROPERTIES['error_debug_mode'] = false;
 
 ?>
