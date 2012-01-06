44c118aeca3fcd30a96c17f7a1abc098373bcbc8
Artyom Grigoriev <ortemij@gmail.com>
2012-01-06 19:47:12 +0400

    fixes in test

--- junk/ortemij/face_detect_test.php
+++ junk/ortemij/face_detect_test.php
@@ -10,6 +10,8 @@ require_once dirname(__FILE__) . '/../../main/classes/utils/Logger.php';
 
 $LOG = new Logger();
 $am = OpenCVAvatarsMinifier::getInstance();
-$am->minify(dirname(__FILE__) . '/' . $argv[0], dirname(__FILE__) . '/' . $argv[1], 100, 100);
+print_r($argv);
+echo dirname(__FILE__), "\n";
+$am->minify(dirname(__FILE__) . '/' . $argv[1], dirname(__FILE__) . '/' . $argv[2], 100, 100);
 
 ?>
