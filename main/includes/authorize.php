4018f5884177e722e40e014b2fb1e53bd30da70e
Artyom Grigoriev <ortemij@gmail.com>
2012-01-18 23:33:17 +0400

    Mobile_Detect class from Google Code

--- main/includes/authorize.php
+++ main/includes/authorize.php
@@ -3,7 +3,14 @@
 require_once dirname(__FILE__) . '/../classes/user/User.php';
 require_once dirname(__FILE__) . '/../classes/user/Auth.php';
 
+require_once dirname(__FILE__) . '/config-local.php';
+
 $auth = new Auth();
 $user = $auth->getCurrentUser();
+
+if ($auth->isMobile()) {
+	Header('Location: ' . MOBILE_SITE_URL);
+	exit(0);
+}
 
 ?>
