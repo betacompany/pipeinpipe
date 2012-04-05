f376aeb9ef7344d1f986311f4ae2b3b5e644ae0f
Boris Smirnov <boris.v.smirnov@gmail.com>
2012-04-05 23:57:54 +0400

    vk graph

--- main/config/local-properties.php
+++ main/config/local-properties.php
@@ -3,4 +3,5 @@
 global $PROPERTIES;
 $PROPERTIES['main_site_url'] = "local.pipeinpipe.info";
 $PROPERTIES['mysql']['first_query'] = 'SET NAMES utf8';
+$PROPERTIES['mysql']['first_query'] = 'SET NAMES utf8';
 ?>
