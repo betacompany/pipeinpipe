a5e2bbc372b3630722013f72eadee753734e1b98
Artyom Grigoriev <ortemij@gmail.com>
2012-04-07 21:18:30 +0400

    Forgetting main/config/local-properties.php

--- main/config/local-properties.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-global $PROPERTIES;
-$PROPERTIES['main_site_url'] = "local.pipeinpipe.info";
-$PROPERTIES['mysql']['first_query'] = 'SET NAMES utf8';
-$PROPERTIES['mysql']['first_query'] = 'SET NAMES utf8';
-?>
