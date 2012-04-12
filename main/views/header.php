66e9f51a8b278ede517e69aeb6caba38364a12fe
Artyom Grigoriev <ortemij@gmail.com>
2012-04-12 09:10:30 +0400

    xmlns:pipe

--- main/views/header.php
+++ main/views/header.php
@@ -213,7 +213,7 @@ if ($_SERVER['SCRIPT_NAME'] == '/life.php') {
 }
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns:pipe="http://pipeinpipe.info">
 	<head>
 		<title><?=$title?></title>
 
