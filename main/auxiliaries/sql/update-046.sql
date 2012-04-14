e338da63ad795133f0111591640d4a7aabf70175
Artyom Grigoriev <ortemij@gmail.com>
2012-04-14 15:28:30 +0400

    update-046.sql fix

--- main/auxiliaries/sql/update-046.sql
+++ main/auxiliaries/sql/update-046.sql
@@ -5,7 +5,7 @@ CREATE TABLE `p_social_post` (
   `title` varchar(128) NOT NULL DEFAULT '',
   `source` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `p_social_post` ADD `handled` INT(11)  NOT NULL  DEFAULT '0'  AFTER `source`;
 
