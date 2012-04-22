eb7bb70867b20cab5f6304bdbd9f1025a2d0d679
Artyom Grigoriev <ortemij@gmail.com>
2012-04-22 19:19:07 +0400

    Font size

--- main/views/life_timeline.php
+++ main/views/life_timeline.php
@@ -122,7 +122,7 @@ function life_show_item_content(Item $item) {
 		
 		<p>
 			<div>
-				<a href="/life/blog/<?=$item->getId()?>"><?=$item->getTitle()?></a>
+				<a style="font-size: 1.2em;" href="/life/blog/<?=$item->getId()?>"><?=$item->getTitle()?></a>
 				<small>+&nbsp;<?=lang_number_sclon($item->countComments(), "комментарий", "комментария", "комментариев")?></small>
 			</div>
 			<?=$item->getShortHTML()?>
