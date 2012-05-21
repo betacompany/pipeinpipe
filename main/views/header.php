f4cada06ae8a95a18628174d71463aeeea4b905b
Artyom Grigoriev <ortemij@gmail.com>
2012-05-21 20:28:51 +0400

    xmlns

--- main/views/header.php
+++ main/views/header.php
@@ -220,7 +220,7 @@ if ($_SERVER['SCRIPT_NAME'] == '/life.php') {
 }
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:pipe="http://pipeinpipe.info">
 	<head>
 		<title><?=$title?></title>
 
